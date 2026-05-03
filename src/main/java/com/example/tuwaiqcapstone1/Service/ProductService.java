ad72e76b68b43fb0a2658aac5b394e5a6317b65f
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-05-03 11:52:48 +0300

    feat: product CRUD endpoints

--- src/main/java/com/example/tuwaiqcapstone1/Service/ProductService.java
+++ src/main/java/com/example/tuwaiqcapstone1/Service/ProductService.java
@@ -1,4 +1,57 @@
 package com.example.tuwaiqcapstone1.Service;
 
+import com.example.tuwaiqcapstone1.Model.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final CategoryService categoryService;
+
+    ArrayList<Product> products = new ArrayList<>();
+
+    //BASIC CRUD ENDPOINTS
+    public ArrayList<Product> getProducts(){
+        return products;
+    }
+
+    public int addProduct(Product product){
+        if(findProductIndex(product.getId()) == -1) {//not found
+            if(categoryService.findCategoryIndex(product.getCategoryId()) == -1) //not found
+                return 0;
+            products.add(product);
+            return 1;
+        }
+        return -1;
+    }
+
+    public boolean updateProduct(String id, Product product){
+        int index = findProductIndex(id);
+        if(index == -1) //not found
+            return false;
+        product.setId(id); //make sure the user doesn't change the id
+        products.set(index, product);
+        return true;
+    }
+
+    public boolean deleteProduct(String id){
+        int index = findProductIndex(id);
+        if(index == -1) //not found
+            return false;
+        products.remove(index);
+        return true;
+    }
+
+
+    //HELPER METHODS
+    public int findProductIndex(String id){
+        for(int i = 0; i < products.size(); i++)
+            if(products.get(i).getId().equalsIgnoreCase(id))
+                return i;
+        return -1;
+    }
 }
