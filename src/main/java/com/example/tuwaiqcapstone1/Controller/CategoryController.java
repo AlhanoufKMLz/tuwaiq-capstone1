45a3afbb4cc9b5a6787bb5eba018c79aa8e3dabf
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-05-03 11:15:11 +0300

    feat: category CRUD endpoints

--- src/main/java/com/example/tuwaiqcapstone1/Controller/CategoryController.java
+++ src/main/java/com/example/tuwaiqcapstone1/Controller/CategoryController.java
@@ -1,4 +1,54 @@
 package com.example.tuwaiqcapstone1.Controller;
 
+import com.example.tuwaiqcapstone1.ApiResponse.ApiResponse;
+import com.example.tuwaiqcapstone1.Model.Category;
+import com.example.tuwaiqcapstone1.Service.CategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/category")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    //BASIC CRUD ENDPOINTS
+    @GetMapping("/get")
+    public ResponseEntity<?> getCategories(){
+        return ResponseEntity.status(200).body(categoryService.getCategories());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addCategory(@RequestBody @Valid Category category, Errors errors){
+        if(errors.hasErrors())
+            return ResponseEntity.status(400).body(new ApiResponse(errors.getFieldError().getDefaultMessage()));
+
+        boolean isDone = categoryService.addCategory(category);
+        if(isDone)
+            return ResponseEntity.status(200).body(new ApiResponse("Category added successfully"));
+        return ResponseEntity.status(400).body(new ApiResponse("ID: " + category.getId() + " already used"));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable String id, @RequestBody @Valid Category category, Errors errors){
+        if(errors.hasErrors())
+            return ResponseEntity.status(400).body(new ApiResponse(errors.getFieldError().getDefaultMessage()));
+
+        boolean isDone = categoryService.updateCategory(id, category);
+        if(isDone)
+            return ResponseEntity.status(200).body(new ApiResponse("Category updated successfully"));
+        return ResponseEntity.status(400).body(new ApiResponse("No category with ID: " + id + " found"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCategory(@PathVariable String id){
+        boolean isDone = categoryService.deleteCategory(id);
+        if(isDone)
+            return ResponseEntity.status(200).body(new ApiResponse("Category deleted successfully"));
+        return ResponseEntity.status(400).body(new ApiResponse("No category with ID: " + id + " found"));
+    }
 }
--- src/main/java/com/example/tuwaiqcapstone1/Service/CategoryService.java
+++ src/main/java/com/example/tuwaiqcapstone1/Service/CategoryService.java
@@ -1,4 +1,51 @@
 package com.example.tuwaiqcapstone1.Service;
 
+import com.example.tuwaiqcapstone1.Model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class CategoryService {
+
+    ArrayList<Category> categories = new ArrayList<>();
+
+    //BASIC CRUD ENDPOINTS
+    public ArrayList<Category> getCategories(){
+        return categories;
+    }
+
+    public boolean addCategory(Category category){
+        if(findCategoryIndex(category.getId()) == -1) {//not found
+            categories.add(category);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateCategory(String id, Category category){
+        int index = findCategoryIndex(id);
+        if(index == -1) //not found
+            return false;
+        category.setId(id); //make sure the user doesn't change the id
+        categories.set(index, category);
+        return true;
+    }
+
+    public boolean deleteCategory(String id){
+        int index = findCategoryIndex(id);
+        if(index == -1) //not found
+            return false;
+        categories.remove(index);
+        return true;
+    }
+
+
+    //HELPER METHODS
+    public int findCategoryIndex(String id){
+        for(int i = 0; i < categories.size(); i++)
+            if(categories.get(i).getId().equalsIgnoreCase(id))
+                return i;
+        return -1;
+    }
 }
