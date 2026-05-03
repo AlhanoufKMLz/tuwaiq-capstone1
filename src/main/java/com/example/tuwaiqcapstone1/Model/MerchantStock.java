2aa73d4e6ce2f74cd115b6ce03463897fec32341
Alhanouf Allazzam <115586263+AlhanoufKMLz@users.noreply.github.com>
2026-05-03 13:17:51 +0300

    feat: merchantStock model

--- src/main/java/com/example/tuwaiqcapstone1/Model/MerchantStock.java
+++ src/main/java/com/example/tuwaiqcapstone1/Model/MerchantStock.java
@@ -1,5 +1,20 @@
 package com.example.tuwaiqcapstone1.Model;
 
-public class MerchantStock
-{
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class MerchantStock {
+
+    @NotEmpty(message = "ID must not be empty")
+    private String id;
+
+    @NotEmpty(message = "Product ID must not be empty")
+    private String productId;
+
+    @NotEmpty(message = "Merchant ID must not be empty")
+    private String merchantId;
+
+    @NotNull(message = "Stock must not be null")
+    private Integer stock;
+
 }
